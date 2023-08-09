a78632fc4f0a6baa7345732f6b53a7ec9b41cfaa
Danny Trejo <danieltrejo112@gmail.com>
2023-08-09 15:33:41 -0500

    url set for login

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -14,6 +14,18 @@ export default function Landing() {
     const [invalid, setinValidEmail] = useState(false);
     const [loading, setLoading] = useState(false);
     const [success, setSuccess] = useState(false);
+    const getURL = () => {
+        let url =
+          process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+          process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+          'http://localhost:3000/'
+        // Make sure to include `https://` when not localhost.
+        url = url.includes('http') ? url : `https://${url}`
+        // Make sure to include a trailing `/`.
+        url = url.charAt(url.length - 1) === '/' ? url : `${url}/`
+        return url
+      }
+      
     const login = async () => {
         validate(email);
        if(res && email !='') {
@@ -28,7 +40,7 @@ export default function Landing() {
             let { data, error } = await supabase.auth.signInWithOtp({
                 email,
                 options: {
-                    emailRedirectTo: 'https://migraine-buddy.vercel.app/',
+                    emailRedirectTo: getURL(),
                   },
 
             });
